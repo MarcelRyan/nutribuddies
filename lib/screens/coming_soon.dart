02d153c92ed59fe5799734cf6de9c11d0e979516
MarcelRyan <marcel.ryan2004@gmail.com>
2024-01-12 15:31:11 +0700

    feat: coming soon page

---
+++ lib/screens/coming_soon.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:nutribuddies/constant/colors.dart';
+import 'package:nutribuddies/widgets/wrapper.dart';
+
+import '../services/auth.dart';
+import '../widgets/loading.dart';
+
+class ComingSoon extends StatelessWidget {
+  const ComingSoon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: background,
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(40, 250, 40, 65),
+        child: Column(
+          children: [
+            Image.asset('assets/coming_soon.png'),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "We're still cooking our app",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400,
+                height: 0.06,
+              ),
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            const Text(
+              "New features coming soon!",
+              textAlign: TextAlign.center,
+              softWrap: true,
+              style: TextStyle(
+                color: Color(0xFF74747E),
+                fontSize: 16,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400,
+                height: 0.09,
+                letterSpacing: 0.50,
+              ),
+            ),
+            const SizedBox(
+              height: 90,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
