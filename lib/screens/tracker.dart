339536c586305279b6d257ac6946a95ab1cdb9b7
MarcelRyan <marcel.ryan2004@gmail.com>
2024-01-12 11:59:51 +0700

    feat: add meal modal

--- lib/screens/tracker.dart
+++ lib/screens/tracker.dart
@@ -765,6 +765,15 @@ class _TrackerContentState extends State<TrackerContent> {
                         spreadRadius: 1,
                       ),
                     ]),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AddMeal()));
+                  },
+                  child: const Text("Add Meal"),
+                ),
               ),
             ],
           ),
