bcc04cf5f1e7db8916d6f68f055257cceb33739b
maikeljh <mjh191003@gmail.com>
2024-01-17 14:03:57 +0700

    fix: styling home page articles

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -555,7 +555,6 @@ class _HomePageState extends State<HomePage> {
             ),
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.345,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -575,9 +574,7 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                       ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.35,
-                      ),
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.35),
                       const FittedBox(
                         fit: BoxFit.scaleDown,
                         child: Text(
@@ -594,125 +591,124 @@ class _HomePageState extends State<HomePage> {
                       )
                     ],
                   ),
-                  Expanded(
-                    child: ListView(
-                      children: dummyData.map((record) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.only(
-                              top: MediaQuery.of(context).size.height * 0.015),
-                          padding: EdgeInsets.symmetric(
-                              horizontal:
-                                  MediaQuery.of(context).size.width * 0.05,
-                              vertical:
-                                  MediaQuery.of(context).size.height * 0.02),
-                          height: MediaQuery.of(context).size.height * 0.14,
-                          decoration: BoxDecoration(
-                            color: surfaceBright,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset: const Offset(0, 2),
-                              ),
-                            ],
-                          ),
-                          child: Row(
-                            children: [
-                              Image.asset(
-                                record.imagePath,
-                                width: MediaQuery.of(context).size.width * 0.2,
-                                fit: BoxFit.fill,
-                              ),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.03,
-                              ),
-                              Expanded(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.005,
+                  const SizedBox(height: 4),
+                  ListView(
+                    shrinkWrap: true,
+                    primary: false,
+                    padding: EdgeInsets.zero,
+                    children: dummyData.map((record) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.015),
+                        padding: EdgeInsets.symmetric(
+                            horizontal:
+                                MediaQuery.of(context).size.width * 0.05,
+                            vertical:
+                                MediaQuery.of(context).size.height * 0.02),
+                        height: MediaQuery.of(context).size.height * 0.14,
+                        decoration: BoxDecoration(
+                          color: surfaceBright,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              record.imagePath,
+                              width: MediaQuery.of(context).size.width * 0.2,
+                              fit: BoxFit.fill,
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.03,
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.005,
+                                  ),
+                                  Text(
+                                    dateFormatter(record.date),
+                                    style: const TextStyle(
+                                      color: Color(0xFF74747E),
+                                      fontSize: 10,
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w500,
+                                      height: 1,
+                                      letterSpacing: 0.50,
                                     ),
-                                    Text(
-                                      dateFormatter(record.date),
-                                      style: const TextStyle(
-                                        color: Color(0xFF74747E),
-                                        fontSize: 10,
-                                        fontFamily: 'Poppins',
-                                        fontWeight: FontWeight.w500,
-                                        height: 1,
-                                        letterSpacing: 0.50,
-                                      ),
-                                      textAlign: TextAlign.start,
+                                    textAlign: TextAlign.start,
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.012,
+                                  ),
+                                  Text(
+                                    record.title,
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w600,
+                                      height: 1.2,
+                                      letterSpacing: 0.15,
                                     ),
-                                    SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.012,
-                                    ),
-                                    Text(
-                                      record.title,
-                                      style: const TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 16,
-                                        fontFamily: 'Poppins',
-                                        fontWeight: FontWeight.w600,
-                                        height: 1.2,
-                                        letterSpacing: 0.15,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.013,
-                                    ),
-                                    Row(
-                                      children: [
-                                        for (int i = 0;
-                                            i < record.tags.length;
-                                            i++) ...[
-                                          if (i == record.tags.length - 1)
-                                            Text(
-                                              record.tags[i],
-                                              style: const TextStyle(
-                                                color: Color(0xFF5674A7),
-                                                fontSize: 11,
-                                                fontFamily: 'Poppins',
-                                                fontWeight: FontWeight.w500,
-                                                height: 1,
-                                                letterSpacing: 0.50,
-                                              ),
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.013,
+                                  ),
+                                  Row(
+                                    children: [
+                                      for (int i = 0;
+                                          i < record.tags.length;
+                                          i++) ...[
+                                        if (i == record.tags.length - 1)
+                                          Text(
+                                            record.tags[i],
+                                            style: const TextStyle(
+                                              color: Color(0xFF5674A7),
+                                              fontSize: 11,
+                                              fontFamily: 'Poppins',
+                                              fontWeight: FontWeight.w500,
+                                              height: 1,
+                                              letterSpacing: 0.50,
                                             ),
-                                          if (i != record.tags.length - 1)
-                                            Text(
-                                              "${record.tags[i]} ",
-                                              style: const TextStyle(
-                                                color: Color(0xFF5674A7),
-                                                fontSize: 11,
-                                                fontFamily: 'Poppins',
-                                                fontWeight: FontWeight.w500,
-                                                height: 1,
-                                                letterSpacing: 0.50,
-                                              ),
+                                          ),
+                                        if (i != record.tags.length - 1)
+                                          Text(
+                                            "${record.tags[i]} ",
+                                            style: const TextStyle(
+                                              color: Color(0xFF5674A7),
+                                              fontSize: 11,
+                                              fontFamily: 'Poppins',
+                                              fontWeight: FontWeight.w500,
+                                              height: 1,
+                                              letterSpacing: 0.50,
                                             ),
-                                        ]
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  )
+                                          ),
+                                      ]
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
                 ],
               ),
             ),
