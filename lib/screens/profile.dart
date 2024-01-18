57858ddd8ef0c904db92085ae26c6fe5b54d43e4
Raylouis <sebastianraynard@gmail.com>
2024-01-18 12:37:37 +0700

    refactor: refactor some code

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:nutribuddies/constant/colors.dart';
 import 'package:nutribuddies/models/kids.dart';
